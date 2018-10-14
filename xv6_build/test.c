5a90d3964348257b66c0f1596abac3bd1c5ca9d6
David Silva <dsilv022@sledge.cs.ucr.edu>
2018-10-14 16:19:16 -0700

    waitpid not recognized

--- xv6_build/test.c
+++ xv6_build/test.c
@@ -1,9 +1,24 @@
 #include "types.h"
 #include "user.h"
+//#include "proc.h"
 
 
-int main(){
-
-printf(1, "Hello World \n");
-exit(0);
+int main() {
+	/*
+	int child_pid = fork();
+	if (child_pid == 0) {
+		printf(1, "I exit first!\n");
+	} else {
+		some sort of waitpid here
+		int returnVal = waitpid(child_pid, 0, 0);
+		if (returnVal > 0) {
+			printf(1, "I exit second because I wait on first to exit!\n");
+		} else {
+			printf(1, "hmm\n");
+		}
+		
+	}
+	*/
+	printf(1, "Hello World \n");
+	exit(0);
 }
